fe66579268669aeaa74273371bdd3fa51b8378ad
luidimso <luidi_mso@outlook.com>
2020-12-14 10:16:00 -0300

    Class contact

---
+++ android/app/src/main/kotlin/br/com/flutter_app_contact_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.flutter_app_contact_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controllers/contact.dart
@@ -0,0 +1,46 @@
+import 'package:sqflite/sqflite.dart';
+
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imageColumn = "imageColumn";
+
+class ContactController {
+
+}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    image = map[image];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imageColumn: image
+    };
+    if(id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, image: $image)";
+  }
+
+}
