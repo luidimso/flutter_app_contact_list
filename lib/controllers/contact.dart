613fa609a666322dd56a9e8bd5964efff5981607
luidimso <luidi_mso@outlook.com>
2020-12-14 11:09:04 -0300

    Starting singleton instance of Contact Controller database class

--- lib/controllers/contact.dart
+++ lib/controllers/contact.dart
@@ -1,13 +1,39 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-final String idColumn = "idColumn";
-final String nameColumn = "nameColumn";
-final String emailColumn = "emailColumn";
-final String phoneColumn = "phoneColumn";
-final String imageColumn = "imageColumn";
+final String contactTable = "contact";
+final String idColumn = "id";
+final String nameColumn = "name";
+final String emailColumn = "email";
+final String phoneColumn = "phone";
+final String imageColumn = "image";
 
 class ContactController {
+  static final ContactController _instance = ContactController.internal();
+  factory ContactController() => _instance;
+  ContactController.internal();
 
+  Database _db;
+
+  Future<Database> get db async {
+    if(_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, "contacts.db");
+
+    return openDatabase(
+        path, version: 1, onCreate: (Database db, int newerVersion) async {
+          await db.execute("CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imageColumn TEXT)");
+        }
+    );
+  }
 }
 
 class Contact {
