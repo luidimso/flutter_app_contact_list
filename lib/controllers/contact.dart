83d7e9b28089c70de6c741661d395c3e92225ff8
luidimso <luidi_mso@outlook.com>
2020-12-17 11:47:02 -0300

    Save e get contact functions

--- lib/controllers/contact.dart
+++ lib/controllers/contact.dart
@@ -34,6 +34,26 @@ class ContactController {
         }
     );
   }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id]
+    );
+    if(maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
 }
 
 class Contact {
